c5a76dc7b5d4c88d8b0148bfcd83a98ad291e8df
Harjan Redegeld <hajredegeld@casema.nl>
2021-05-13 16:31:04 +0200

    Renamed variables in age.js, changes to pf.js. Changes to CSS and added stuff in index.html.

--- js/age.js
+++ js/age.js
@@ -1,13 +1,13 @@
-var today = new Date();
-var birthDate = new Date("1998/03/04");
-var age = today.getFullYear() - birthDate.getFullYear();
-var m = today.getMonth() - birthDate.getMonth();
-if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+var currentDate = new Date();
+var birthDate = new Date('1998/03/04');
+var age = currentDate.getFullYear() - birthDate.getFullYear();
+var month = currentDate.getMonth() - birthDate.getMonth();
+if (month < 0 || (month === 0 && currentDate.getDate() < birthDate.getDate())) {
 age--;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("age").innerHTML = age.toString() + "/M | he/him";
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('age').innerHTML = age.toString() + '/M | he/him';
 })
 
 
--- js/pf.js
+++ js/pf.js
@@ -1,19 +1,38 @@
-var picPaths = ['img/picture1.jpg','img/picture2.png','img/picture5.jpg','img/picture4.jpg', 'img/picture3.jpg', 'img/picture8.jpg', 'img/picture7.jpg', 'img/picture6.jpg'];
-var curPic = -1;
+var images = [
+    'img/picture1.jpg',
+    'img/picture2.png',
+    'img/picture5.jpg',
+    'img/picture4.jpg',
+    'img/picture3.jpg',
+    'img/picture8.jpg',
+    'img/picture7.jpg',
+    'img/picture6.jpg'
+];
 
-var imgO = new Array();
-for(i=0; i < picPaths.length; i++) {
-imgO[i] = new Image();
-imgO[i].src = picPaths[i];
+var gallery_index = -1;
+var gallery_switching = false;
+var gallery_second = true;
+
+function galleryNext() {
+    if (gallery_switching)
+        return;
+
+    gallery_switching = true;
+
+    gallery_index = (gallery_index + 1) % images.length;
+
+    document.getElementById(gallery_second ? 'gallery_a' : 'gallery_b').src = images[gallery_index];
+
+    gallery_second = !gallery_second;
 }
 
-function swapImage() {
-curPic = (++curPic > picPaths.length-1)? 0 : curPic;
-imgCont.src = imgO[curPic].src;
-setTimeout(swapImage,3000);
+function galleryLoaded() {
+    if (!gallery_switching)
+        return;
+    gallery_switching = false;
+
+    document.getElementById('gallery_b').style.opacity = gallery_second ? 1 : 0;
 }
 
-window.onload=function() {
-    imgCont = document.getElementById('profile');
-    swapImage();
-}
+setTimeout(galleryNext, 0); // Fake "onload" here
+setInterval(galleryNext, 2500); // Switch once every 5 seconds
