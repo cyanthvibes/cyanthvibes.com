a6fd59894f73f73d9831d4a979ffc12784a3e63f
Harjan Redegeld <hajredegeld@casema.nl>
2021-05-20 20:59:20 +0200

    Changes to JS, CSS and HTML for profile pictures switching

--- js/pf.js
+++ js/pf.js
@@ -1,42 +1,10 @@
-var images = [
-    'img/picture1.jpg',
-    'img/picture2.png',
-    'img/picture5.jpg',
-    'img/picture4.jpg',
-    'img/picture3.jpg',
-    'img/picture8.jpg',
-    'img/picture7.jpg',
-    'img/picture6.jpg'
-];
+window.addEventListener("DOMContentLoaded", function(e) {
 
-var gallery_index = -1;
-var gallery_switching = false;
-var gallery_second = true;
-
-function galleryNext() {
-    if (gallery_switching)
-        return;
-
-    gallery_switching = true;
-
-    gallery_index = (gallery_index + 1) % images.length;
-
-    try {
-        document.getElementById(gallery_second ? 'gallery_a' : 'gallery_b').src = images[gallery_index];
-    } catch (e) {
-        document.getElementById(gallery_second ? 'gallery_a' : 'gallery_b').src = images[gallery_index];
+    var galleryImages = document.getElementById("gallery-images");
+    var fadeComplete = function(e) { galleryImages.appendChild(arr[0]); };
+    var arr = galleryImages.getElementsByTagName("img");
+    for(var i=0; i < arr.length; i++) {
+        arr[i].addEventListener("animationend", fadeComplete, false);
     }
 
-    gallery_second = !gallery_second;
-}
-
-function galleryLoaded() {
-    if (!gallery_switching)
-        return;
-    gallery_switching = false;
-
-    document.getElementById('gallery_b').style.opacity = gallery_second ? 1 : 0;
-}
-
-setTimeout(galleryNext, 0); // Fake "onload" here
-setInterval(galleryNext, 3000); // Switch once every 6 seconds
+}, false);
